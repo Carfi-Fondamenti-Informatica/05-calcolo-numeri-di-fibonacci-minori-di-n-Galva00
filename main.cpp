3e46bc6655f4bf96559e5c19138c2df4fc5eb8b4
Galva00 <116543265+Galva00@users.noreply.github.com>
2022-11-27 18:10:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-   int n=0, a=1, b=1, c=0;
-   cin >> n;
-   cout << a << endl << b << endl;
-   for (int i=0; i<=n; i++){
-      c = a + b;
-      a = b;
-      b = c;
-   }
-      cout << c << endl;
-   return 0;
+    int n=0, a=1, b=1, f=0;
+    cin >> n;
+    cout << a << endl << b << endl;
+    while (f<n) {
+        f = a + b;
+        a = b;
+        b = f;
+        cout << f << endl;
+    }
+    return 0;
 }
