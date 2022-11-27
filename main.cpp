9238c991097fea6b16e0efbd945884bf40282ba6
Galva00 <116543265+Galva00@users.noreply.github.com>
2022-11-27 17:45:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main()
+{
+   int n;
+   cin >> n;
+
+   if (n < 2)
+   {
+      cout << "errore";
+      return 0;
+   };
+   int a = 1, b = 0, c = 0;
+   for (int i = 1; i <= n; i++)
+   {
+      c = a + b;
+      a = b;
+      b = c;
+
+      cout << c << endl;
+   }
 }
